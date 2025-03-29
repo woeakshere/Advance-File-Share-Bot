48955b7ed16c82440082a15b9252da2bba41f2a6
woeakshere <dangersaitama@gmail.com>
2025-03-29 17:28:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Wleakshere:Thunderstrikes27@wleakshere.api7w.mongodb.net/?retryWrites=true&w=majority&appName=Wleakshere")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Clust")
+DATABASE_NAME = environ.get('DATABASE_NAME', "advancew")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # FSUB
